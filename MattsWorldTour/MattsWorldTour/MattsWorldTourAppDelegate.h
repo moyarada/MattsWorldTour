8665b4da9db104f4a88f44517b9e481b4ee9bdf7
Morten Gregersen <mig@mortengregersen.dk>
2011-04-29 22:55:42 +0200

    Second try

---
+++ MattsWorldTour/MattsWorldTour/MattsWorldTourAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  MattsWorldTourAppDelegate.h
+//  MattsWorldTour
+//
+//  Created by Morten Gregersen on 4/29/11.
+//  Copyright 2011 MoGee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MattsWorldTourViewController;
+
+@interface MattsWorldTourAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet MattsWorldTourViewController *viewController;
+
+@end
---
+++ MattsWorldTour/MattsWorldTour/MattsWorldTourViewController.h
@@ -0,0 +1,15 @@
+//
+//  MattsWorldTourViewController.h
+//  MattsWorldTour
+//
+//  Created by Morten Gregersen on 4/29/11.
+//  Copyright 2011 MoGee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MattsWorldTourViewController : UIViewController {
+    
+}
+
+@end
