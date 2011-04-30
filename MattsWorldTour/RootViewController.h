df9b0019561b4285ef69138220a2b506c9a13b0e
Morten Gregersen <mig@mortengregersen.dk>
2011-04-30 16:35:35 +0200

    Supported orientations specified RootViewController's view is now a scrollview

---
+++ MattsWorldTour/RootViewController.h
@@ -0,0 +1,15 @@
+//
+//  RootViewController.h
+//  MattsWorldTour
+//
+//  Created by Morten Gregersen on 4/29/11.
+//  Copyright 2011 CCDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController {
+    
+}
+
+@end
