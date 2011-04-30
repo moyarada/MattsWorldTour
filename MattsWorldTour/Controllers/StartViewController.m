283e9f2ec389d12a384db25621265c5594dfca11
Morten Bøgh <morten@hugelawn.com>
2011-05-01 00:35:49 +0200

    Hmm

--- MattsWorldTour/Controllers/StartViewController.m
+++ MattsWorldTour/Controllers/StartViewController.m
@@ -61,7 +61,7 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-	return YES;
+	return ((interfaceOrientation == UIInterfaceOrientationLandscapeLeft) || (interfaceOrientation == UIInterfaceOrientationLandscapeRight));
 }
 
 @end
